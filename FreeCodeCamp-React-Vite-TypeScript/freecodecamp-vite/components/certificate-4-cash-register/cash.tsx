b8c1a8dec1390e62a7d378feac688bfe2f17a278
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2025-03-22 19:25:12 +0700

    .

--- FreeCodeCamp-React-Vite-TypeScript/freecodecamp-vite/components/certificate-4-cash-register/cash.tsx
+++ FreeCodeCamp-React-Vite-TypeScript/freecodecamp-vite/components/certificate-4-cash-register/cash.tsx
@@ -1,8 +1,95 @@
+import { ChangeEvent, useState } from "react";
 
-import React from 'react';
+type CurrencyName =
+  | "PENNY"
+  | "NICKEL"
+  | "DIME"
+  | "QUARTER"
+  | "ONE"
+  | "FIVE"
+  | "TEN"
+  | "TWENTY"
+  | "ONE HUNDRED";
+type CurrencyTuple = [CurrencyName, number];
+type cidCash = CurrencyTuple[];
 
+const cid: cidCash = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+];
 
+interface resultCash {
+  status: string;
+  change: never[];
+}
+const result: resultCash = {
+  status: "OPEN",
+  change: [],
+};
+
+const price: number = 3.26;
 function CashRegister() {
+  const [value, setValue] = useState<number>(0);
+  const [change, setChange] = useState<string>("");
+  const [showResult, setShowResult] = useState<object>(result);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    const numeric = inputValue === "" ? 0 : parseFloat(inputValue);
+    if (!isNaN(numeric)) {
+      setValue(numeric);
+    }
+  };
+
+  const handlePurchase = () => {
+    /* để giá tiền khách hàng và giá trong két về tròn số 100 */
+    const cashInCents = Math.round(Number(value) * 100);
+    const priceInCents = Math.round(price * 100);
+    console.log(cashInCents);
+    /* Điều kiện nếu số tiền khacsh khách hàng đưa ít hơn giá tiền thối */
+    console.log(priceInCents);
+    if (cashInCents < priceInCents) {
+      alert("Customer does not have enough money to purchase the item");
+      return;
+    } else {
+      setValue(value);
+    }
+    /* điều kiện nếu số tiền khách hàng bằng số tiền trong két */
+    if (cashInCents === priceInCents) {
+      setChange("No change due - customer paid with exact cash");
+    } else {
+      setValue(value);
+    }
+    /* tiền khách - trừ tiền thối */
+    const changeDue = cashInCents - priceInCents;
+    const revesedCid: CurrencyTuple[] = [...cid]
+      .reverse()
+      .map(([item, amount]) => [item, Math.round(amount * 100)]);
+    const denominations: number[] = [10000, 2000, 1000, 500, 100, 25, 10, 5, 1];
+    if (
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      changeDue > revesedCid.reduce((prev, [_, amount]) => prev + amount, 0)
+    ) {
+      setShowResult({ status: "INSUFFICIENT_FUNDS", change: [] });
+      return;
+    }
+    for(let i = 0; i < revesedCid.length; i++) {
+      if(changeDue >= denominations[i] && changeDue >0) {
+        const [denominations,amount] = revesedCid[i];
+        const possibleChange = Math.min(amount, changeDue);
+        const count = Math.floor(possibleChange / denominations[i]);
+            const amountInChange = count * denominations[i];
+      }
+    }
+  };
+
   return (
     <main className="cash">
       <img
@@ -11,11 +98,23 @@ function CashRegister() {
         alt=""
       />
       <h1>Cash Register Project</h1>
-      <div id="change-due"></div>
+      <div id="change-due">
+        <p>{change}</p>
+      </div>
       <div className="input-div">
         <label htmlFor="cash">Enter cash from customer:</label>
-        <input type="number" value="" className="user-input" id="cash" />
-        <button className="check-btn-styles" id="purchase-btn">
+        <input
+          type="number"
+          onChange={handleChange}
+          value={value}
+          className="user-input"
+          id="cash"
+        />
+        <button
+          onClick={handlePurchase}
+          className="check-btn-styles"
+          id="purchase-btn"
+        >
           Purchase
         </button>
       </div>
